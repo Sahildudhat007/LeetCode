e16da0cb083a65087db3b53719eef8c21ba1eb2a
Sahil <sahildudhat1919@gmail.com>
2024-02-17 09:41:43 +0530

    17-02-2024

---
+++ Prectice/Divide.js
@@ -0,0 +1,8 @@
+{
+    function divide(a, b) {
+        // console.log(a/b);
+        return a / b;
+    }
+    console.log(divide(10, 3));
+    console.log(divide(7, -3));
+}
---
+++ Prectice/Sqrt.js
@@ -0,0 +1,7 @@
+{
+    let math1 = Math.sqrt(4)
+    console.log(math1);
+    
+    let math2 = Math.sqrt(8);
+    console.log(math2);
+}
